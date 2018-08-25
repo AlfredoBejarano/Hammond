292a06645238c7010310a99ac39e540118a49d47
AlfredoBejarano <alfredocbejarano@gmail.com>
2018-08-25 18:26:41 -0500

    Update FingerprintReader.kt
    Add functions to open and close connections to the fingerprint sensor

--- hammond/src/main/java/com/alfredobejarano/hammond/receiver/FingerprintReader.kt
+++ hammond/src/main/java/com/alfredobejarano/hammond/receiver/FingerprintReader.kt
@@ -2,8 +2,7 @@ package com.alfredobejarano.hammond.receiver
 
 import android.arch.lifecycle.MutableLiveData
 import android.content.Context
-import android.hardware.usb.UsbDevice
-import android.hardware.usb.UsbManager
+import android.hardware.usb.*
 import com.alfredobejarano.hammond.receiver.model.Device
 
 /**
@@ -25,6 +24,26 @@ object FingerprintReader {
     var usbDevice: UsbDevice? = null
 
     /**
+     * Reference to this sensor USB interface.
+     */
+    var usbInterface: UsbInterface? = null
+
+    /**
+     * Reference to this sensor USB read endpoint.
+     */
+    var usbReadEndpoint: UsbEndpoint? = null
+
+    /**
+     * Reference to this sensor USB write endpoint.
+     */
+    var usbWriteEndpoint: UsbEndpoint? = null
+
+    /**
+     * Reference to this sensor USB connection.
+     */
+    var usbDeviceConnection: UsbDeviceConnection? = null
+
+    /**
      * Checks if a supported fingerprint reader device is currently connected.
      * @return true if a supported device is connected.
      */
@@ -44,5 +63,29 @@ object FingerprintReader {
         }
         // If the connected device is not null, return true.
         return usbDevice != null
+    }
+
+    /**
+     * Opens a connection to the Fingerprint sensor.
+     */
+    private fun open(ctx: Context) {
+        // Retrieve the UsbManager from the system services.
+        val manager = ctx.getSystemService(Context.USB_SERVICE) as UsbManager?
+        // Retrieve the USB device interface.
+        val usbDeviceInterface = usbDevice?.getInterface(0)
+        // Try to perform a connection to the device.
+        Device.attemptConnection(manager, usbDeviceInterface)
+    }
+
+    /**
+     * Removes all references to this sensor USB properties and reports its disconnected status.
+     */
+    fun close() {
+        usbDevice = null
+        usbInterface = null
+        usbReadEndpoint = null
+        usbWriteEndpoint = null
+        usbDeviceConnection = null
+        status.postValue(DeviceStatus.STATUS_DISCONNECTED)
     }
 }
